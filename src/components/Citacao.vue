2a57bcd2f6c4a860130edf30898fd982864a0d80
JaimeDevS <jaime_mendes91@yahoo.com.br>
2025-02-04 22:14:47 -0300

    Componentes dinamicos

--- src/components/Citacao.vue
+++ src/components/Citacao.vue
@@ -2,7 +2,9 @@
     <div class="citacao">
         <slot name="fonte" ></slot>
         <slot></slot>
-        <slot name="autor"></slot>
+        <div class="autor">
+            <slot name="autor"></slot>
+        </div>
     </div>
 </template>
 
@@ -18,5 +20,9 @@ export default {
         background-color: rgba(0, 0, 0, .1);
         padding: 25px;
         max-width: 450px;
+    }
+
+    .autor {
+        float: right;
     }
 </style>
