bb8d00bba757f891aaf40ba1604940ea1d15fe71
JaimeDevS <jaime_mendes91@yahoo.com.br>
2025-02-06 20:11:42 -0300

    Adicionada observações

--- src/components/Citacao.vue
+++ src/components/Citacao.vue
@@ -10,7 +10,22 @@
 
 <script>
 export default {
-    
+    created() {
+        console.log('created')
+    },
+    destroyed() {
+        console.log('cerated')
+    },
+    /**
+     * os métodos activated e deactivated
+     * só vão funcionar quando o keep-alive estiver definido
+     */
+    activated() {
+        console.log('activated')
+    },
+    deactivated() {
+        console.log('deactivated')
+    },
 }
 </script>
 
